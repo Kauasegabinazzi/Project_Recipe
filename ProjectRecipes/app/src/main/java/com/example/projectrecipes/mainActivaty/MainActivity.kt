9a2d6d816ede198713036429f10e8f865be4f438
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-09-29 15:57:51 -0300

    6-commit

---
+++ ProjectRecipes/app/src/main/java/com/example/projectrecipes/mainActivaty/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.example.projectrecipes.mainActivaty
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.example.projectrecipes.R
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
