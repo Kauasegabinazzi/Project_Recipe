243c111e8f4455a529b71b7d2839b107c73a92c6
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-10-01 22:42:21 -0300

    16-commit

--- ProjectRecipes/app/src/main/java/com/example/projectrecipes/Details/Details.kt
+++ ProjectRecipes/app/src/main/java/com/example/projectrecipes/Details/Details.kt
@@ -10,12 +10,13 @@ import com.example.projectrecipes.R
 class Details : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_details)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_details)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
 }
--- ProjectRecipes/app/src/main/java/com/example/projectrecipes/Exit/Exit.kt
+++ ProjectRecipes/app/src/main/java/com/example/projectrecipes/Exit/Exit.kt
@@ -10,12 +10,13 @@ import com.example.projectrecipes.R
 class Exit : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_exit)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        enableEdgeToEdge()
+//        setContentView(R.layout.activity_exit)
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
 }
