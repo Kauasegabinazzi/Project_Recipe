f5fa6eba597e72455bcfcab6a842e33b8f0060d4
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-10-01 23:31:24 -0300

    17-commit

--- ProjectRecipes/app/src/main/java/com/example/projectrecipes/Details/Details.kt
+++ ProjectRecipes/app/src/main/java/com/example/projectrecipes/Details/Details.kt
@@ -1,5 +1,6 @@
 package com.example.projectrecipes.Details
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,7 +8,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.projectrecipes.R
 
-class Details : AppCompatActivity() {
+class Details : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_details)
--- ProjectRecipes/app/src/main/java/com/example/projectrecipes/mainActivaty/MainActivity.kt
+++ ProjectRecipes/app/src/main/java/com/example/projectrecipes/mainActivaty/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.example.projectrecipes.mainActivaty
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.example.projectrecipes.Details.Details
 import com.example.projectrecipes.R
 
 class MainActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Obtenha o botão pelo ID
+        val loginButton: Button = findViewById(R.id.loginButton)
+
+        // Defina um listener para o clique do botão
+        loginButton.setOnClickListener {
+            // Crie uma Intent para iniciar a Details activity
+            val newProduct = "Example Product"
+            val intent = Intent(this, Details::class.java)
+            startActivity(intent)
+        }
     }
 }
