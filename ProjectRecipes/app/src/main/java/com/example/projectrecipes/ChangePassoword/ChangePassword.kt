0c4157a107be4809461385ee3a177b7e20d0666f
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-10-03 23:37:42 -0300

    26-commit

--- ProjectRecipes/app/src/main/java/com/example/projectrecipes/ChangePassoword/ChangePassword.kt
+++ ProjectRecipes/app/src/main/java/com/example/projectrecipes/ChangePassoword/ChangePassword.kt
@@ -1,16 +1,22 @@
 package com.example.projectrecipes.ChangePassoword
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.Button
 import com.example.projectrecipes.R
+import com.example.projectrecipes.mainActivaty.MainActivity
 
 class ChangePassword : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_change_password)
+
+        val changePasswordCard2 = findViewById<Button>(R.id.loginButton)
+
+        changePasswordCard2.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
--- ProjectRecipes/app/src/main/java/com/example/projectrecipes/Exit/Exit.kt
+++ ProjectRecipes/app/src/main/java/com/example/projectrecipes/Exit/Exit.kt
@@ -3,27 +3,29 @@ package com.example.projectrecipes.Exit
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.cardview.widget.CardView
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.EditText
 import com.example.projectrecipes.ChangePassoword.ChangePassword
-import com.example.projectrecipes.Details.Details2
 import com.example.projectrecipes.R
+import com.example.projectrecipes.mainActivaty.MainActivity
 
 class Exit : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exit)
 
-//        val changePasswordCard = findViewById<CardView>(R.id.changePasswordCard)
-//
-//        changePasswordCard.setOnClickListener {
-//            val intent = Intent(this, ChangePassword::class.java)
-//            startActivity(intent)
-//        }
+        val changePasswordCard = findViewById<EditText>(R.id.senha)
+
+        changePasswordCard.setOnClickListener {
+            val intent = Intent(this, ChangePassword::class.java)
+            startActivity(intent)
+        }
+
+        val changePasswordCard2 = findViewById<EditText>(R.id.sair)
+
+        changePasswordCard2.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
