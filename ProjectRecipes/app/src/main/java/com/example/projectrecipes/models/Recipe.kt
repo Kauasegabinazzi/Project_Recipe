ab594cb546bd513e1991643b24d4635cd1d5489d
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-10-06 22:23:19 -0300

    27-commit

--- ProjectRecipes/app/src/main/java/com/example/projectrecipes/models/Recipe.kt
+++
@@ -1,9 +0,0 @@
-class NumbersView // create constructor to set the values for all the parameters of the each single view
-    (// getter method for returning the ID of the imageview
-    // the resource ID for the imageView
-    val numbersImageId: Int, // getter method for returning the ID of the TextView 1
-    // TextView 1
-    val numberInDigit: String, // getter method for returning the ID of the TextView 2
-    // TextView 1
-    val numbersInText: String
-) 
