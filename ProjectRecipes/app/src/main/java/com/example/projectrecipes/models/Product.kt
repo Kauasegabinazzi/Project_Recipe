2915f7d7023e46acbccc75941b16b3e82bdc731f
Kauasegabinazzi <kauasegabinazzi1234@gmail.com>
2024-11-01 20:18:01 -0300

    commit- aula

--- ProjectRecipes/app/src/main/java/com/example/projectrecipes/models/Product.kt
+++ ProjectRecipes/app/src/main/java/com/example/projectrecipes/models/Product.kt
@@ -4,3 +4,6 @@ package com.example.projectrecipes.models
 data class Product (
         val name: String,
         val description: String)
+
+data class User(val username: String, val passwordHash: String)
+data class UsersList(val users: List<User>)
