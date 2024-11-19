1e72e2dd918486aadd817100a87aa37aab6ff250
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-11-18 23:35:43 -0300

    44-commit

--- ProjectRecipes/app/build.gradle.kts
+++ ProjectRecipes/app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -63,7 +64,8 @@ dependencies {
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
     implementation(libs.firebase.crashlytics.buildtools)
-    implementation(libs.google.gson)  // Gson dependency
+    implementation(libs.google.gson)
+    implementation(libs.firebase.auth)  // Gson dependency
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
