63c02e64ae8a60087a45f50d7016d9fc8bc35201
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-11-19 22:20:53 -0300

    46-commit

--- ProjectRecipes/app/src/main/java/com/example/projectrecipes/mainActivaty/MainActivity.kt
+++ ProjectRecipes/app/src/main/java/com/example/projectrecipes/mainActivaty/MainActivity.kt
@@ -9,11 +9,16 @@ import com.example.projectrecipes.Details.Details
 import com.example.projectrecipes.ProductsListAdapter
 import com.example.projectrecipes.R
 import com.example.projectrecipes.models.Product
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : Activity() {
+
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        firebaseAuth = FirebaseAuth.getInstance()
 
         val loginButton: Button = findViewById(R.id.loginButton)
 
