a18208ffa51e29cba495d741327bb01d70427ab2
Kauasegabinazzi <101999858+Kauasegabinazzi@users.noreply.github.com>
2024-11-20 20:39:25 -0300

    49-commit

--- ProjectRecipes/app/src/main/java/com/example/projectrecipes/Home_Details/Home_Details.kt
+++ ProjectRecipes/app/src/main/java/com/example/projectrecipes/Home_Details/Home_Details.kt
@@ -1,5 +1,6 @@
 package com.example.projectrecipes.Home_Details
 
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -7,15 +8,15 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.projectrecipes.R
 
-class Home_Details : AppCompatActivity() {
+class Home_Details : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+//        enableEdgeToEdge()
         setContentView(R.layout.activity_home_details)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+//            insets
+//        }
     }
 }
--- ProjectRecipes/app/src/main/java/com/example/projectrecipes/ProductsListAdapter.kt
+++ ProjectRecipes/app/src/main/java/com/example/projectrecipes/ProductsListAdapter.kt
@@ -1,11 +1,15 @@
 package com.example.projectrecipes
 
 import android.content.Context
+import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.example.projectrecipes.Details.Details2
+import com.example.projectrecipes.Home_Details.Home_Details
 import com.example.projectrecipes.models.Product
 
 class ProductsListAdapter(private val context: Context, private val products: List<Product>) : RecyclerView.Adapter<ProductsListAdapter.ViewHolders>() {
@@ -21,9 +25,22 @@ class ProductsListAdapter(private val context: Context, private val products: Li
     override fun onBindViewHolder(holder: ProductsListAdapter.ViewHolders, position: Int) {
         val product = products[position]
         holder.vincula(product)
+
+        holder.loginButton.setOnClickListener {
+            // Cria uma Intent para navegar para `Details2` ao clicar no botão
+            val intent = Intent(context, Home_Details::class.java)
+            // (Opcional) Passe dados extras se necessário
+            intent.putExtra("product_name", product.name)
+            intent.putExtra("product_description", product.description)
+            context.startActivity(intent)
+        }
+
     }
 
     class ViewHolders(view: View) : RecyclerView.ViewHolder(view) {
+
+        val loginButton: Button = itemView.findViewById(R.id.myButton)
+
         fun vincula(product: Product) {
             val name = itemView.findViewById<TextView>(R.id.name)
             name.text = product.name
